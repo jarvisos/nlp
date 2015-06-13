38e4d003492f68f1d6f2fe10a56d3f1fd5f55854
W. Max Lees <william.lees@echostar.com>
2015-06-13 14:18:32 -0600

    Added License to files

--- network/sigmoidNeuronLayer.go
+++ network/sigmoidNeuronLayer.go
@@ -1,3 +1,26 @@
+/*
+  Copyright 2015 W. Max Lees
+
+  This file is part of jarvisos.
+
+  Jarvisos is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Jarvisos is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with jarvisos.  If not, see <http://www.gnu.org/licenses/>.
+
+  File: sigmoidNeuronLayer.go
+  Author: W. Max Lees <max.lees@gmail.com>
+  Date: 06.12.2015
+*/
+
 package network
 
 import (
